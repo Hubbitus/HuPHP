5f8a9dfa6e0a6df4a4120800ccc9026122ff181f
Pavel Alexeev <Pahan@Hubbitus.info>
2009-03-01 11:19:06 +0000

    Add macroses IS_SET and IS_SET_VAR
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@37 38b62f15-a12a-4f48-9651-19e9dcdd08cb

--- macroses/ISSET_VAR.php
+++ macroses/ISSET_VAR.php
@@ -3,26 +3,48 @@
 * Toolkit of small functions as "macroses".
 *
 * @package Macroses
-* @version 1.0
+* @version 1.1
 * @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
 * @copyright Copyright (c) 2008, Pahan-Hubbitus (Pavel Alexeev)
+*
 * @changelog
 *	* 2009-01-30 15:10 ver 1.0
+*	- Initial version
+*
+*	* 2009-03-01 13:59 ver 1.0 to 1.1
+*	- Add second, "safe" variant of macros IS_SET_VAR. It is based on self
+*		{@see is_set} instead of standard isset. See overall description why in it.
+*	- Old ISSET_VAR can't be switched on IS_SET, because use 1 incoming parameter.
+*		Also, it must be used fo just variables (not check indexes in array or strings)
 **/
 
 /**
-* Return value of variable if it defined without notices and error-handling.
+* Return value of SCALAR variable if it defined without notices and error-handling.
+* For safely check indexes (in string and arrays use {@see IS_SET_VAR})
 *
-* In most cases check like "if ($variable)" is laconic form of more strict like "if (isset($variable) and $variable)".
+* In most cases check like "if ($variable) $str = $variable . 'some'" is laconic form of more strict like "if (isset($variable) and $variable) $str = $variable . 'some'".
 * So, if $variable was not defined yet we got notice. Well, when we do not need it, we can suppress it like "if (@$variable)"
-* all seems good on first glance but we only supress error message, NOT error processing if it occures!
+* all seems good on first glance but we only supress error message, NOT error handling if it occures!
 * So, if error handler was be set before (like set_error_handler("func_error_handler");) this error handler got control and stack will be broken!
+*
+* With that function we may safely use simple: $str = ISSET_VAR($variable) . 'some'...
+*
+* For Chec
 *
 * @param &mixed	$var variable amount of arguments.
 * @return &mixed
 **/
 function &ISSET_VAR(&$var){
 	if (isset($var)) return $var;
+	else{
+	$t = null; //To do not fire error "Only variables can be passed by reference in ..."
+	return $t;
+	}
+}
+
+function &IS_SET_VAR($what, &$where){
+include_once('macroses/IS_SET.php');//MUST be explicit. It used in autoload.php, so, autoloading is not present yet!
+	if (is_set($what, $where)) return $where[$what];
 	else{
 	$t = null; //To do not fire error "Only variables can be passed by reference in ..."
 	return $t;
---
+++ macroses/IS_SET.php
@@ -0,0 +1,46 @@
+<?
+/**
+* Toolkit of small functions as "macroses".
+*
+* @package Macroses
+* @version 1.0
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
+* @copyright Copyright (c) 2008, Pahan-Hubbitus (Pavel Alexeev)
+*
+* @changelog
+*	* 2009-03-01 13:57 ver 1.0
+*	- Initial version
+**/
+
+/**
+* Intended to USE ANYWERE INSTEAD OF ISSET!!!
+* Return TRUE isset, correct handling strings opposite official version.
+*
+* See PHP bugs:
+* http://bugs.php.net/bug.php?id=43889
+* http://bugs.php.net/bug.php?id=38165
+* http://bugs.php.net/bug.php?id=29883
+* http://bugs.php.net/bug.php?id=26413
+*
+* Thay want think this is "future" (I think it is a BUG!) of PHP.
+* When we want check presented key in array we MUST do there 3 checks if check in array due to the
+* ($str = 'text'; isset($str['any index']) <- Always return true!!!)
+* instead of just check: if ( isset($GLOBALS['__CONFIG'][$classname]['class_file']) ) :
+*	isset($GLOBALS['__CONFIG'][$classname])
+*	and is_array($GLOBALS['__CONFIG'][$classname])
+*	and isset($GLOBALS['__CONFIG'][$classname]['class_file'])
+* OR use this dirty hack, which shortly, and does the same:
+*	in_array('class_file', (array)@$GLOBALS['__CONFIG'][$classname])
+* OR can check on string...
+*
+* Last variant is safele for all cases as I think. So, implement it.
+*
+* @param string	$what Key
+* @param &(string|array)	$where Where check.
+* @return boolean
+**/
+function is_set($what, &$where){# Opposite to standard isset.
+	if (is_string($where) and !is_numeric($what)) return false;
+	else return isset($where[$what]);
+}
+?>
