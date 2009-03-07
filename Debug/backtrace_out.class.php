f50fdb7a19ee5d33786609f2c658a20a3db57f81
Pavel Alexeev <Pahan@Hubbitus.info>
2009-03-07 15:37:58 +0000

    Add new class backtrace_out
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@64 38b62f15-a12a-4f48-9651-19e9dcdd08cb

---
+++ Debug/backtrace_out.class.php
@@ -0,0 +1,31 @@
+<?
+/**
+* Debug and backtrace toolkit.
+* Class to provide convenient backtrace logging.
+*
+* @package Debug
+* @subpackage Bactrace
+* @version 1.0
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
+* @copyright Copyright (c) 2008, Pahan-Hubbitus (Pavel Alexeev)
+*
+* @changelog
+*	* 2009-03-07 18:35 ver 1.0
+*	- Initial add.
+**/
+
+include_once('Vars/commonOutExtraData.php');
+
+class backtrace_out extends commonOutExtraData{
+	public function strToConsole($format = nul){
+	return $this->_var->printout(true, null, OS::OUT_TYPE_CONSOLE);
+	}#m strToConsole
+
+	public function strToFile($format = null){
+	return $this->_var->printout(true, null, OS::OUT_TYPE_FILE);
+	}#m strToFile
+
+	public function strToWeb($format = null){
+	return $this->_var->printout(true, null, OS::OUT_TYPE_BROWSER);
+	}#m strToWeb
+}#c
