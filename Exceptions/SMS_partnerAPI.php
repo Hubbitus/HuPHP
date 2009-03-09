b20e2ed0524c1f6c270f7cbdd282816b2c3349ae
Pavel Alexeev <Pahan@Hubbitus.info>
2009-03-09 17:58:48 +0000

    *	* 2009-03-09 20:58 ver 1.0 *	- Initial import into Hubbitus repository
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@82 38b62f15-a12a-4f48-9651-19e9dcdd08cb

---
+++ Exceptions/SMS_partnerAPI.php
@@ -0,0 +1,22 @@
+<?
+/**
+* O-Range-SMS-partnerAPI ralated exceptions.
+*
+* @package Exceptions
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
+* @copyright Copyright (c) 2008, Pahan-Hubbitus (Pavel Alexeev)
+* @version 1.0
+*
+* @changelog
+*	* 2009-03-09 20:58 ver 1.0
+*	- Initial import into Hubbitus repository
+**/
+
+include_once('Exceptions/BaseException.php');
+
+class MSG_partnerAPIException extends BaseException{}
+
+class MSG_AuthErrorException extends MSG_partnerAPIException{}
+class MSG_SendFailException extends MSG_partnerAPIException{}
+class MSG_InParseErrorException extends MSG_partnerAPIException{}
+?>
