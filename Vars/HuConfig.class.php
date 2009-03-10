7e3d3619ea766a20ce8d96182910e8a02a2b7120
Pavel Alexeev <Pahan@Hubbitus.info>
2009-03-10 02:01:20 +0000

    *	* 2009-03-10 04:59 ver 0.3 to 0.4 *	- Add try to autoinclude config: 'includes/configs/' . $name . '.config.php' if it is untill not present in $GLOBALS['__CONFIG']
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@85 38b62f15-a12a-4f48-9651-19e9dcdd08cb

--- Vars/HuConfig.class.php
+++ Vars/HuConfig.class.php
@@ -4,7 +4,7 @@
 *
 * @package Vars
 * @subpackage Settings
-* @version 0.3
+* @version 0.4
 * @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
 * @copyright Copyright (c) 2009, Pahan-Hubbitus (Pavel Alexeev)
 *
@@ -18,11 +18,16 @@
 *	* 2009-03-09 05:31 ver 0.2 to 0.3
 *	- Add optional parameter $className to CONF() function!
 *	- @subpackage changed to Settings
+*
+*	* 2009-03-10 04:59 ver 0.3 to 0.4
+*	- Add try to autoinclude config: 'includes/configs/' . $name . '.config.php' if it is untill not present in $GLOBALS['__CONFIG']
 **/
 
 //It used for __autoload, so, we must directly prowide dependencies here
 include_once('Vars/Settings/settings_check.php');
 include_once('Vars/HuArray.php');
+include_once('System/OS.php'); // OS::is_includable
+include_once('macroses/REQUIRED_VAR.php');
 
 /**
 * Class to provide easy access to $GLOBALS['__CONFIG'] variables.
@@ -30,6 +35,7 @@ include_once('Vars/HuArray.php');
 * @example Single::def('HuConfig')->config_value
 **/
 class HuConfig extends settings_check{
+private $_include_tryed = array();
 	function __construct() {
 	parent::__construct(array_keys($GLOBALS['__CONFIG']), $GLOBALS['__CONFIG']);
 	}#__c
@@ -37,13 +43,14 @@ class HuConfig extends settings_check{
 	/**
 	* As __get before.
 	* Now {@see __get()} reimplemented to return HuArray instead of raw arrays
+	* Bee careful - after standard call (not raw) original Array value was replaced by HuArray!
 	*
 	* @param string	$varname
 	* @return &mixed
 	**/
 	public function &getRaw($varname){
 	return $this->getProperty($varname);
-	}#getRaw
+	}#m getRaw
 
 	/**
 	* For more comfort access in config fields without temporary variables like:
@@ -59,14 +66,34 @@ class HuConfig extends settings_check{
 		return $ret;
 		}
 		else return $ret;
-	}
+	}#m __get
 
 	/**
 	* Reimplement as initial, only return value by reference
+	* Also try include file 'includes/configs/' . $name . '.config.php' if it exist to find needed settings.
 	* @inheritdoc
 	**/
-	public function &getProperty($name){
-	return $this->__SETS[$this->checkNamePossible(REQUIRED_NOT_NULL($name), __METHOD__)];
+	public function &getProperty($name){	
+		try{
+		return $this->__SETS[$this->checkNamePossible(REQUIRED_NOT_NULL($name), __METHOD__)];
+		}
+		catch(ClassPropertyNotExistsException $cpne){
+			//Try include appropriate file:
+			if (!in_array($name, $this->_include_tryed)){
+			$this->_include_tryed[] = $name; //In any case to do not check again next time
+				if(OS::is_includeable(($path = 'includes/configs/' . $name . '.config.php'))){
+//				dump::a($path);
+				include($path);
+					if(m()->is_set($name, $GLOBALS['__CONFIG'])){//New key
+					$this->addSetting($name, $GLOBALS['__CONFIG'][$name]);
+					}
+				//return $this->getProperty($name); //Again
+				return $this->__SETS[$name];
+				//return $this->__SETS[$this->checkNamePossible(REQUIRED_NOT_NULL($name), __METHOD__)];
+				}
+			}
+		throw $cpne; //If include and fine failed throw outside;
+		}
 	}#m getProperty
 }#c
 
@@ -89,9 +116,17 @@ class HuConfig extends settings_check{
 * @return Single_Object(HuConfig)|Object(HuArray). If className present - Object(HuArray) returned, Single_Object(HuConfig) otherwise to next query.
 **/
 function &CONF($className = null){
-	if ($className) return Single::def('HuConfig')->$className;
+	/*
+	* Strange, but if we direct return:
+	* if ($className) return Single::def('HuConfig')->$className;
+	* All work as expected and variable returned by reference, but notice occured:
+	* PHP Notice:  Only variable references should be returned by reference in /var/www/_SHARED_/Vars/HuConfig.class.php on line 111
+	* implicit call to __get solve problem. Is it bug?
+	* @todo Fill bug
+	**/
+	if ($className) return Single::def('HuConfig')->__get($className);
 	else return Single::def('HuConfig');
-}
+}#f CONF
 
 /**
 * @example
