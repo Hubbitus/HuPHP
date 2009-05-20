3a8491dd36318cda576372033f15c6406e093b98
Pavel Alexeev <Pahan@Hubbitus.info>
2009-05-20 15:48:33 +0000

    Introduce DOMnodeOutExtraData class
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@141 38b62f15-a12a-4f48-9651-19e9dcdd08cb

---
+++ Vars/DOMnodeOutExtraData.php
@@ -0,0 +1,56 @@
+<?
+/**
+* Debug and backtrace toolkit.
+*
+* Class to provide easy wrapper aroun HuFormat for anywhere usage.
+*
+* @package Debug
+* @subpackage HuLOG
+* @version 1.0
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
+* @copyright Copyright (c) 2009, Pahan-Hubbitus (Pavel Alexeev)
+*
+* @changelog
+*	* 2009-05-20 19:10 ver 1.0
+*	- Initial version.
+**/
+
+/**
+* Class to provide easy wrapper to dump DOMElement (and DOMnode possibly), which default dump seems like:
+* object(DOMElement)#97 (0) {
+* }
+* This wrapper put it into DOMDocument, and autput it as formated XML. For output standard family of dump::* methods used.
+*
+* @uses commonOutExtraData
+* @uses dump
+**/
+class DOMnodeOutExtraData extends commonOutExtraData{
+protected /* DOMDocument */ $_var;
+	/**
+	* Constructor.
+	*
+	* @param	mixed	$var Var to output with provided format.
+	* @param	array	$format	Format how output $vavr. Must contain 3 elements:
+	*	'FORMAT_CONSOLE', 'FORMAT_WEB', 'FORMAT_FILE' each represent according
+	*	format (See class {@see HuFormat} for more details).
+	**/
+	function  __construct(DOMElement $var, $encoding = 'utf-8'){
+	$this->_var = new DOMDocument('1.0', $encoding); // DOMDocument NEEDED ot import into it nodes, it also NEEDED to export result asXML...
+	$this->_var->appendChild($this->_var->importNode($var, true));
+	$this->_var->preserveWhiteSpace = false;
+	$this->_var->formatOutput = true;
+	}#__c
+
+	public function strToConsole($format = null){
+	return dump::c(trim($this->_var->saveXML()), null, true);
+	}#m strToConsole
+
+	public function strToFile($format = null){
+	return dump::log(trim($this->_var->saveXML()), false, true);
+	}#m strToFile
+
+	public function strToWeb($format = null){
+	return dump::w(trim($this->_var->saveXML()), false, true);
+	}#m strToWeb
+}#c huFormatOutExtraData
+?>
