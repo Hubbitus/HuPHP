cbf6be1f4fb9cfd5679d743dabd6b7f8a1985e79
Pavel Alexeev <Pahan@Hubbitus.info>
2009-05-21 12:17:01 +0000

    Add support of abstract classes. Autoinclude map regenarated
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@150 38b62f15-a12a-4f48-9651-19e9dcdd08cb

--- .tools/auto_extract_class_map.bash
+++ .tools/auto_extract_class_map.bash
@@ -16,13 +16,14 @@ find .. -type f -iname '*.php' \
 	-not -wholename "${DIR}Debug/Phar/*" -not -name 'exec.php' -not -name $( basename "$OUT_FILE" ) \
 	-not -name RegExp_base.php -not -name try-examples.php -not -name '*.example.php' -not -name autoload.php \
 	-not -name template_class_NEW2.php \
-	-exec egrep -iH '^\s*class|function.*{' {} \; | \
+	-exec egrep -iH '^\s*(abstract)?\s*class|function.*{' {} \; | \
 	while read line; do
 	echo $line;
-	echo $line | sed -nr "/function/d;s@${DIR}(.*?\\.php):\s*class\s+([^[:space:]{]+)(\s+extends\s+[^[:space:]{]+)?(\s+implements\s+[^[:space:]]+)*\s*\{?\}?\s*(\$|(//|#).*|;)\$@\t'\2'\t=> '\1',@g;p" \
+	echo $line | sed -nr "/function/d;s@${DIR}(.*?\\.php):\s*(abstract)?\s*class\s+([^[:space:]{]+)(\s+extends\s+[^[:space:]{]+)?(\s+implements\s+[^[:space:]]+)*\s*\{?\}?\s*(\$|(//|#).*|;)\$@\t'\3'\t=> '\1',@g;p" \
 		| grep -v 'Debug/log_dump.php' >> "$OUT_FILE"
 	echo $line; # This must be processed after whole cycle, to filter dupes and do it only once
-	done | sed -nr "s@(${DIR}.*\\.php):.*?@\1@g;p" | sort | uniq | xargs -r -I{} ./phpsource.extract_functions.php "{}" "$DIR" >> "$OUT_FILE"
+	done \
+		| sed -nr "s@(${DIR}.*\\.php):.*?@\1@g;p" | sort | uniq | xargs -r -I{} ./phpsource.extract_functions.php "{}" "$DIR" >> "$OUT_FILE"
 
 echo -n ");
 ?>" >> "$OUT_FILE"
