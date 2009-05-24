77094972a5186e5b0d911fb523de85cc96348422
Pavel Alexeev <Pahan@Hubbitus.info>
2009-05-24 22:24:18 +0000

    1) $line escape to prevent "*" expand 2) exclude phpsource.extract_functions.php from processing 3) Add comment in autogenerated file: This is automatically generated file! Please, do not edit it manual!!! 4) Add "interface" support
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@153 38b62f15-a12a-4f48-9651-19e9dcdd08cb

--- .tools/auto_extract_class_map.bash
+++ .tools/auto_extract_class_map.bash
@@ -5,7 +5,7 @@ OUT_FILE=${OUT_FILE:-"$DIR/__autoload.map.php"}
 
 : > "$OUT_FILE"
 #{
-echo "<?
+echo "<? //This is automatically generated file! Please, do not edit it manual!!!
 \$GLOBALS['__CONFIG']['__autoload_map'] = array(" >> "$OUT_FILE"
 
 # RegExp_base.php contains Function definition in Eval as hack. so, explicit exclude it.
@@ -15,15 +15,17 @@ echo "<?
 find .. -type f -iname '*.php' \
 	-not -wholename "${DIR}Debug/Phar/*" -not -name 'exec.php' -not -name $( basename "$OUT_FILE" ) \
 	-not -name RegExp_base.php -not -name try-examples.php -not -name '*.example.php' -not -name autoload.php \
-	-not -name template_class_NEW2.php \
-	-exec egrep -iH '^\s*(abstract)?\s*class|function.*{' {} \; | \
-	while read line; do
-	echo $line;
-	echo $line | sed -nr "/function/d;s@${DIR}(.*?\\.php):\s*(abstract)?\s*class\s+([^[:space:]{]+)(\s+extends\s+[^[:space:]{]+)?(\s+implements\s+[^[:space:]]+)*\s*\{?\}?\s*(\$|(//|#).*|;)\$@\t'\3'\t=> '\1',@g;p" \
-		| grep -v 'Debug/log_dump.php' >> "$OUT_FILE"
-	echo $line; # This must be processed after whole cycle, to filter dupes and do it only once
-	done \
-		| sed -nr "s@(${DIR}.*\\.php):.*?@\1@g;p" | sort | uniq | xargs -r -I{} ./phpsource.extract_functions.php "{}" "$DIR" >> "$OUT_FILE"
+	-not -name template_class_NEW2.php -not -name 'phpsource.extract_functions.php' \
+	-exec egrep -iH '^\s*((abstract)?\s*class.+{|interface.+{)|function.+\(' {} \; | \
+		while read line; do
+		echo "$line"; # This will be processed after whole cycle, to filter dupes and do it only once
+		#echo ========
+
+		echo "$line" | sed -nr "/function/d;/class/s@${DIR}(.*?\\.php):\s*(abstract\s+)?class\s+([^[:space:]{]+)(\s+extends\s+[^[:space:]{]+)?(\s+implements\s+[^[:space:]{]+)*\s*\{?\}?\s*(\$|(//|#).*|;)\$@\t'\3'\t=> '\1',@g;/interface/s@${DIR}(.*?\\.php):\s*interface\s+([^\s{]+)\s*\{@\t'\2'\t=> '\1', #interface@g;p" \
+			>> "$OUT_FILE"
+		done \
+			| sed -nr "s@(${DIR}.*\\.php):.*?@\1@g;p" | sort | uniq | xargs -r -I{} ./phpsource.extract_functions.php "{}" "$DIR" \
+				>> "$OUT_FILE"
 
 echo -n ");
 ?>" >> "$OUT_FILE"
