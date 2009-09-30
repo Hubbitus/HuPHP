1a258e1c2fbbe37f4f67cc02c117b7e8e1aab79d
Pavel Alexeev <Pahan@Hubbitus.info>
2009-09-30 10:16:38 +0000

    Introduce new macros DEFINED_CLASS
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@206 38b62f15-a12a-4f48-9651-19e9dcdd08cb

---
+++ macroses/DEFINED_CLASS.php
@@ -0,0 +1,30 @@
+<?
+/**
+* Toolkit of small functions as "macroses".
+*
+* @package Macroses
+* @version 1.0
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan [at] Hubbitus [ dot. ] info>
+* @copyright Copyright (c) 2009, Pahan-Hubbitus (Pavel Alexeev)
+**/
+
+/**
+* Return name of first defined class from names comes as arguments. Nothing return otherwise (yes, warning will).
+*
+* Common usage: $classname = DEFINED_CLASS('some_child', 'base'); $obj = new $classname();
+*
+* @params	variable amount of arguments - strings of class name to try.
+* @return	string|null
+**/
+function DEFINED_CLASS(){
+$numargs = func_num_args();
+$i=0;
+	while (
+		$i < $numargs
+		 and
+		!( $res = class_exists($classname = func_get_arg($i++)) )
+	){/*Nothing do, just skip it */}
+
+	if ($res) return $classname;
+}
+?>
