078da62abc118b9e5549f31b2aff274071374228
Pavel Alexeev <Pahan@Hubbitus.info>
2010-04-02 20:46:53 +0000

    Addd macros EMPTY_callback
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@244 38b62f15-a12a-4f48-9651-19e9dcdd08cb

---
+++ macroses/EMPTY_callback.php
@@ -0,0 +1,31 @@
+<?
+/**
+* Toolkit of small functions as "macroses".
+*
+* @package Macroses
+* @version 1.0
+* @author Pahan-Hubbitus (Pavel Alexeev) <Pahan@Hubbitus.info>
+* @copyright Copyright (c) 2010, Pahan-Hubbitus (Pavel Alexeev)
+* @since 2010-04-03 00:31
+**/
+
+/**
+* Return first argument after @call forr what true === call($arg). Nothing return otherwise (yes, warning will).
+* It is logick continue of set macroses like EMPTY_INT, EMPTY_STR, EMPTY_VAR...
+*
+* @param	callback	$call
+* @params	variable amount of arguments to check.
+* @return mixed
+**/
+function EMPTY_callback($call){
+$numargs = func_num_args();
+$i = 1; //0 is callback
+	while (
+		$i < $numargs
+		 and
+		!($res = call_user_func($call, $arg = func_get_arg($i++)))
+	){/*Nothing do, just skip it */}
+
+	if ($res) return $arg;
+}#f EMPTY_callback
+?>
