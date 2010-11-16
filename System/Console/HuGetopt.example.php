c4cf2bdd68f8a828f6c1b685ccd2945492afd9ea
Pavel Alexeev <Pahan@Hubbitus.info>
2010-11-16 15:58:41 +0000

    Add in example shoer option usage without space
    git-svn-id: svn+ssh://x-www.info/svn/_SHARED_/trunk@255 38b62f15-a12a-4f48-9651-19e9dcdd08cb

--- System/Console/HuGetopt.example.php
+++ System/Console/HuGetopt.example.php
@@ -9,23 +9,24 @@ include ('HuGetopt.php');
 **/
 
 $argv = array(
-  0 => "HuGetopt.php",
+  0 => 'HuGetopt.php',
   1 => '-internet', //NOT option, Must handle properly and appear in nonoptions array
-  2 => "./replace_in_file.php",
-  3 => "--what",
-  4 => "/test/",
-  5 => "--what",
-  6 => "/test2 of regexp/",
-  7 => "--what=text test 3",	//= form
-  8 => "--to",
-  9 => "QAZ",
-  10 => "-i",
-  11 => "replace_in_file.test.text",
-  12 => "/home/pasha/bin/filmInfoFormat.php",
-  13 => "/home/pasha/bin/fsck.vfat.recode.php",
-  14 => "/home/pasha/bin/mozila_get_extension_info.php",
-  15 => "/home/pasha/bin/replace_in_file.php",
-  16 => "/home/pasha/bin/SIM_history.php",
+  2 => './replace_in_file.php',
+  3 => '--what',
+  4 => '/test/',
+  5 => '--what',
+  6 => '/test2 of regexp/',
+  7 => '--what=text test 3',	//= form
+  8 => '--to',
+  9 => 'QAZ',
+  10 => '-i',
+  11 => 'replace_in_file.test.text',
+  12 => '/home/pasha/bin/filmInfoFormat.php',
+  13 => '/home/pasha/bin/fsck.vfat.recode.php',
+  14 => '/home/pasha/bin/mozila_get_extension_info.php',
+  15 => '/home/pasha/bin/replace_in_file.php',
+  16 => '/home/pasha/bin/SIM_history.php',
+  17 => '-wFile' # Short without space
 );
 dump::a($argv);
 
